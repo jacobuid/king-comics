11a895ed98d301c4b3ed6755caf8f786fb2ab682
Jacob King <1899414+jacobuid@users.noreply.github.com>
2026-08-17 00:07:18 -0500

    Hydrate prerendered pages in the app root

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig({
     preact({
       prerender: {
         enabled: true,
+        renderTarget: '#root',
         additionalPrerenderRoutes: [
           '/',
           '/signup',
